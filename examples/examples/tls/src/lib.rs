3229ceee142c09164f8b8f8e3c0eca51b336997c
WillChilds-Klein <willck93@gmail.com>
2024-04-24 22:30:48 +0000

    Revert "Create new AWS-LC-based PQ CryptoMode"
    This reverts commit 5c089a446ab28b76707cd76a74a9702af282153d.

--- examples/examples/tls/src/lib.rs
+++ examples/examples/tls/src/lib.rs
@@ -11,7 +11,9 @@ Then, uses TLS 1.3 to make a sample call to AWS Key Management Service (AWS KMS)
 This example assumes you have set up environment variables for authentication.
 
 */
-use aws_config::BehaviorVersion;
+//use aws_config::BehaviorVersion;
+use aws_smithy_runtime_api::client::behavior_version::BehaviorVersion;
+//use aws_sdk_kms::Error;
 
 use aws_smithy_experimental::hyper_1_0::{CryptoMode, HyperClientBuilder};
 
@@ -20,8 +22,9 @@ pub async fn connect_via_tls_13() -> Result<(), aws_sdk_kms::Error> {
 
     rustls_post_quantum::provider().install_default().unwrap();
 
+    // feature = crypto-aws-lc
     let http_client = HyperClientBuilder::new()
-        .crypto_mode(CryptoMode::PQ)
+        .crypto_mode(CryptoMode::AwsLc)
         .build_https();
 
     let shared_conf = aws_config::defaults(BehaviorVersion::latest())
@@ -30,7 +33,10 @@ pub async fn connect_via_tls_13() -> Result<(), aws_sdk_kms::Error> {
         .await;
 
     let kms_client = aws_sdk_kms::Client::new(&shared_conf);
-    let response = kms_client.list_keys().send().await?;
+    let response = kms_client
+        .list_keys()
+        .send()
+        .await?;
 
     println!("{:?}", response);
 
--- sdk/aws-smithy-experimental/src/hyper_1_0.rs
+++ sdk/aws-smithy-experimental/src/hyper_1_0.rs
@@ -53,18 +53,13 @@ pub enum CryptoMode {
     AwsLc,
     #[cfg(feature = "crypto-aws-lc-fips")]
     AwsLcFips,
-    #[cfg(feature = "crypto-pq")]
-    PQ,
 }
 
 impl CryptoMode {
     fn provider(self) -> CryptoProvider {
         match self {
             #[cfg(feature = "crypto-aws-lc")]
-            CryptoMode::AwsLc => rustls::crypto::aws_lc_rs::default_provider(),
-
-            #[cfg(feature = "crypto-pq")]
-            CryptoMode::PQ => CryptoProvider {
+            CryptoMode::AwsLc => CryptoProvider {
                 kx_groups: vec![
                     &rustls_post_quantum::X25519Kyber768Draft00,
                     rustls::crypto::aws_lc_rs::kx_group::X25519,
@@ -137,11 +132,6 @@ mod cached_connectors {
         HttpsConnector<HttpConnector>,
     > = once_cell::sync::Lazy::new(|| make_tls(GaiResolver::new(), CryptoMode::AwsLc.provider()));
 
-    #[cfg(feature = "crypto-pq")]
-    pub(crate) static HTTPS_NATIVE_ROOTS_AWS_PQ: once_cell::sync::Lazy<
-        HttpsConnector<HttpConnector>,
-    > = once_cell::sync::Lazy::new(|| make_tls(GaiResolver::new(), CryptoMode::PQ.provider()));
-
     #[cfg(feature = "crypto-aws-lc-fips")]
     pub(crate) static HTTPS_NATIVE_ROOTS_AWS_LC_FIPS: once_cell::sync::Lazy<
         HttpsConnector<HttpConnector>,
@@ -155,8 +145,6 @@ mod cached_connectors {
             Inner::Standard(CryptoMode::Ring) => HTTPS_NATIVE_ROOTS_RING.clone(),
             #[cfg(feature = "crypto-aws-lc")]
             Inner::Standard(CryptoMode::AwsLc) => HTTPS_NATIVE_ROOTS_AWS_LC.clone(),
-            #[cfg(feature = "crypto-pq")]
-            Inner::Standard(CryptoMode::PQ) => HTTPS_NATIVE_ROOTS_AWS_PQ.clone(),
             #[cfg(feature = "crypto-aws-lc-fips")]
             Inner::Standard(CryptoMode::AwsLcFips) => HTTPS_NATIVE_ROOTS_AWS_LC_FIPS.clone(),
             #[allow(unreachable_patterns)]
